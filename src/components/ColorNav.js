4d48a6ce2cbce7398742982ca3f51c3693e41221
rmagley <rachelmagley@gmail.com>
2022-01-05 15:45:45 -0500

    complete colornav logic for clean hex

--- src/components/ColorNav.js
+++ src/components/ColorNav.js
@@ -1,31 +1,65 @@
-import React, { useContext } from 'react';
+import React, { useContext , useState} from 'react';
 import { ColorContext } from './colorContext';
 
 
 function ColorNav(props) {
 
     const {color, setColor} = useContext(ColorContext);
+    console.log(color);
+    let [tempColor, setTempColor] = useState('#0047AB');
+    // console.log(tempColor);
+    let tempColorArr = [...tempColor.split("")]
+    // console.log(tempColorArr)
+    let tempColorArr2 = tempColorArr.splice(0, 1) ;
+    // console.log(tempColorArr2)
+    // console.log(tempColorArr)
+    // console.log(tempColorArr.join(''));
+    tempColor = (tempColorArr.join(''))
+    setColor(tempColor)
+    console.log(color)
+
 
     return (
-        <div>
+			<div>
+				<h2>Select your color!</h2>
 
-            <h2>Select your color!</h2>
-
-            <p style={{ color: color }}>
-               Color Selected: {color}
-            </p>
-            <label>
-                Click Here to Select Color:{" "}
-                <input
-                type="color"
-                onChange={(event) =>
-                    setColor(event.target.value)  
-                }
-                />
-            </label>
-
-        </div>
-    );
+				<p style={{ color: color }}>Color Selected: {color}</p>
+				<label>
+					Click Here to Select Color:{' '}
+					<input
+						type='color'
+						onChange={(event) => setTempColor(event.target.value)}
+					/>
+				</label>
+			</div>
+		);
 }
-
 export default ColorNav;
+
+// function ColorNav(props) {
+
+//     const {color, setColor} = useContext(ColorContext);
+//     console.log(color);
+
+
+//     return (
+//         <div>
+
+//             <h2>Select your color!</h2>
+
+//             <p style={{ color: color }}>
+//                Color Selected: {color}
+//             </p>
+//             <label>
+//                 Click Here to Select Color:{" "}
+//                 <input
+//                 type="color"
+//                 onChange={(event) =>
+//                     setColor(event.target.value)  
+//                 }
+//                 />
+//             </label>
+
+//         </div>
+//     );
+// }
