92c70cbc37741f7fcb7ae9470a253180bf63945b
rmagley <rachelmagley@gmail.com>
2022-01-05 12:46:04 -0500

    dev test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import Triad from './components/Triad';
+import Quad from './components/Quad';
+import Analogic from './components/Analogic';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
 
       <Triad />
+      <Quad />
+      <Analogic />
+      <Home />
 
     </div>
   );
--- src/components/Quad.js
+++ src/components/Quad.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Quad(props) {
     return (
         <div>
-            
+            hello from quad
         </div>
     );
 }
